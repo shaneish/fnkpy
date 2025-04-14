9d7569aec3804cd573fb4fa037ef570385496524
H62756 <h62756@JJ4DNV2JYM.tld>
2025-04-14 16:59:41 -0400

    rewrite to be cleaner, simpler, and to allow multiple inline maps/filters without multiple iterations

--- README.md
+++ README.md
@@ -5,127 +5,42 @@
 A command line interface with functional tendencies that allows you to parse information in the shell using Python.
 
 ## examples
-
-**note on defining closures/lambdas**
-Functions (`-f` and `--function`) can be defined using a variety of function formats, one of them being the default Rust format for closures and another explicitly showing the mapping.  The below are all valid ways to write your functions (each example does the same thing, add one to a list of input numbers):
-  1) `|x: int| x + 1`
-  2) `x: int -> x + 1`
-  3) `(x: int) -> x + 1`
-  4) `|x: int| -> x + 1`
-I prefer the top format shown in 1), but you do you boo boo.  Examples below are written in the mixed form show in 4) above just to make it absolutely clear what the variable and function declarations are.
-
-Note on the note: if you want to treat the input data as strings, you don't need to specify type.  For example:
-  `|s| -> s + "concat to end of s"`
-Additionally, if you're using a uniform type for all variables, you can also just specify the type using the `-t`/`--type` flag:
-  `fnk map -f '|n| -> n + 1' -t int`
-The above function is functionally equivalent to the examples shown in 1 - 4).
+when providing functions for maps and filters, the default variable used is `_`.  so long as you want to use the default variable, you can simply supply functions with that (ie, `_ + 1` , `len(_)`, etc).
 
 #### add one to each input number and print
-```bash
-echo -e "1\n2\n3" | fnk map -f '|n: int| -> n + 1'
+```console
+echo -e "1\n2\n3" | fnk -m '_ + 1'
 >>> 2
 >>> 3
 >>> 4
 ```
-#### add one to each input number and sum
-```bash
-echo -e "1\n2\n3" | fnk map -f '|n: int| -> n + 1' | fnk agg -f sum
->>> 11
-```
 #### take list of full paths and filter out just the parent dir and current file name
-```bash
+```console
 ls -d $PWD/*
 >>> ~/scripts/fnpy/fnk/fnk.py
 >>> ~/scripts/fnpy/fnk/parser.py
 
-ls -d $PWD/* | fnk map -f '|f: str| -> "/".join(f.split(".")[-1].split("/")[-2:])'
+ls -d $PWD/* | fnk -m '"/".join(_.split(".")[-1].split("/")[-2:])'
 >>> fnk/fnk
 >>> fnk/parser
 ```
-#### take input and split each word into it's own line
-```bash
-echo -e "words on a line\nand between lines" | fnk map --split_whitespace
->>> words
->>> on
->>> a
->>> line
->>> and
->>> between
->>> lines
-```
 #### filter out filenames that are .py files
-```bash
+```console
 ls
 >>> fnk.py
 >>> parser.py
 >>> README.md
 >>> random_text.txt
 
-ls | fnk filter -f '|s: str| ".py" in s'
+ls | fnk -f '".py" in _'
 >>> fnk.py
 >>> parser.py
 ```
 #### removed duplicate lines from input data
-```bash
-echo -e "same\nsame\nsame\ndifferent" | fnk map -container_type set
+```console
+echo -e "same\nsame\nsame\ndifferent" | fnk -C set -m '_' -e # collect into set with `-C set`, map nothing with `-m _`, and expand output with `-e`
 >>> same
 >>> different
 ```
-#### read input as json, filter name `name` key, filter out entries without a `name` key, and prepend with "Name: "
-```bash
-echo '[{"name": "shane", "age": 35}, {"name": "dan", "age": 31}, {"age": 22}]' | fnk filtermap -f '|entry| -> "Name: " + entry.get("name", "")' --json_in
->>> shane
->>> dan
-```
-#### read current list of files, import `os` module, and use to delete everything with "tempnvim" in it
-
-```bash
-ls
->>> c:usersstephlocaltempvnim.alk1231l
->>> c:userstephlocaltempnvim.asdl213k1
->>> fnk.py
->>> parser.py
-
-ls | fnk filter -f '|f| -> "tempnvim" in f' | fnk apply -f '|f| os.remove(f)' --modules os
->>> fnk.py
->>> parser.py
-```
-
-#### evaluate a simple expression from the command line
-If you want to manually input your input data instead of piping it in, you can do that as follows:
-```bash
-# note that both functions below are functionally equivalent, eval just evaluates each line of input.
-fnk eval "1 + 1"
->>> 2
-
-fnk filtermap "1 + 1" -f "|e| eval(e)"
->>> 2
-```
-
-#### calculate stats on a string of numbers from the command line
-```bash
-fnk agg "1,2,3" -f stats --input_separator ,
->>> Mean   -> 2.0
->>> Median -> 2.0
->>> Mode   -> 1.0
->>> Stdev  -> 1.0
->>> Var    -> 1.0
-
-echo "1 2 3" | fnk agg -f stats --split_whitespace
->>> Mean   -> 2.0
->>> Median -> 2.0
->>> Mode   -> 1.0
->>> Stdev  -> 1.0
->>> Var    -> 1.0
-
-echo -e "1\n2\n3" | fnk agg -f stats
->>> Mean   -> 2.0
->>> Median -> 2.0
->>> Mode   -> 1.0
->>> Stdev  -> 1.0
->>> Var    -> 1.0
-```
-
-
 ## future
 defo needs unit tests written and defo needs multithreading support bcuz python is slow slow slow.
--- fnk/fnk.py
+++ fnk/fnk.py
@@ -1,178 +1,255 @@
-#!/usr/bin/env python3.11
 import sys
-import os
-from argparse import ArgumentParser
-from stat import S_ISFIFO
-from fnk.parser import Fn
-import pkg_resources
+from importlib import metadata
+from typing import Any, Sequence, Self, Iterable
+from argparse import ArgumentParser, Namespace, Action
+from importlib import import_module
+from enum import Enum
+from dataclasses import dataclass
 
 
-_CMD_NAME = "fnk"
-# _CMD_VERSION = pkg_resources.get_distribution(_CMD_NAME).version
-_CMD_VERSION = "0.1.0"
-_DEFAULT_EXPR = ""
-_DEFAULT_INPUT_LINE_SEPARATOR = "\n"
-_DEFAULT_OUTPUT_LINE_SEPARATOR = "\n"
-if S_ISFIFO(os.fstat(0).st_mode):
-    _DEFAULT_EXPR = sys.stdin.read()
-else:
-    if sys.platform.startswith("win"):
-        _DEFAULT_INPUT_LINE_SEPARATOR = "\\n"
+class Imports(Enum):
+    variable = "VARIABLE"
+    module = "MODULE"
 
 
-def main():
+class Status(Enum):
+    VALID = 0
+    ERROR = 1
+    FILTER = 2
+
+
+class Collection(Enum):
+    SET = set
+    LIST = list
+    TUPLE = tuple
+
+    @classmethod
+    def infer(cls, repr: str) -> Self | None:
+        if repr:
+            match repr.lower():
+                case '[]' | 'list':
+                    return Collection.LIST
+                case '{}' | 'set':
+                    return Collection.SET
+                case '()' | 'tuple':
+                    return Collection.TUPLE
+                case _:
+                    return None
+
+
+@dataclass
+class Record:
+    val: Any
+    status: Status
+
+    def update(self, **kwargs) -> Self:
+        for k in self.__dataclass_fields__.keys():
+            self.__dict__[k] = kwargs.get(k) or self.__dict__[k]
+        return self
+
+
+class OrderedArgsAction(Action):
+    def __call__(
+        self,
+        parser: ArgumentParser,
+        namespace: Namespace,
+        values: str | Sequence[str] | None,
+        option_string: str | None = None,
+    ):
+        if "ordered_args" not in namespace:
+            setattr(namespace, "ordered_args", [])
+        previous = namespace.ordered_args
+        previous.append((self.dest, values))
+        setattr(namespace, "ordered_args", previous)
+
+
+def parse_imports(imports: list[str] | None, import_type: Imports) -> Any:
+    imported = {}
+    if imports:
+        for statement in imports:
+            name = statement
+            val = statement
+            if '=' in statement:
+                name, val = statement.split("=")[:2]
+            if import_type == Imports.module:
+                imported[name] = import_module(val)
+            else:
+                imported[name] = eval(val)
+    return imported
+
+
+def parse_args() -> Namespace:
     parser = ArgumentParser(
-        prog=_CMD_NAME,
+        prog="fnk",
         description="Small CLI tool to help you manipulate shell data with Python commands.",
-    )
-    parser.add_argument(
-        "fn",
-        type=str,
-        default="eval",
-        help="Main command you want to apply to your input. Acceptable inputs are `map`, `apply`, `agg`, `sort`, `fold`, and `filter`.",
-    )
-    parser.add_argument(
-        "expr",
-        type=str,
-        nargs="?",
-        default=_DEFAULT_EXPR,
-        help="Expression you want evaluate using Python.",
     )
     parser.add_argument(
         "-v",
         "--version",
         action="store_true",
-        help=f"Show current version of {_CMD_NAME}",
+        help="Show current version of fnkpy",
     )
     parser.add_argument(
+        "--filter",
         "-f",
-        "--function",
+        action=OrderedArgsAction,
         type=str,
-        default="|v: str| -> v",
-        help="Lambda function to apply or evaluate.  Uses the following closure format: `|x: str, y: int| -> f(x, y)`. Defaults to the identity lambda.  For `fn` value of `agg`, use the name of the function which reduces a collection to a single value.",
+        help="Filter to apply to input stream.",
     )
     parser.add_argument(
-        "-n",
-        "--no_split",
+        "--map",
+        "-m",
+        action=OrderedArgsAction,
+        type=str,
+        help="Map to apply to input stream.",
+    )
+    parser.add_argument(
+        "-C",
+        "--collect",
+        type=str,
+        help="Collection type to collect all lines into prior to applying maps or evals"
+    )
+    parser.add_argument(
+        "--constants",
+        "-c",
+        type=str,
+        nargs="*",
+        help="Additional constants to add to execution namespace.  Should be supplied in format `--variables var_name=var_val 'extra_string=Hello there' two=2`",
+    )
+    parser.add_argument(
+        "--variable-repr",
+        "-V",
+        type=str,
+        default="_",
+        help="Variable name to use across stages, default is '_'",
+    )
+    parser.add_argument(
+        "--imports",
+        "-i",
+        type=str,
+        nargs="*",
+        help="Additional modules to import into execution namespace.  Should be supplied in format `--imports os json=js`",
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
         action="store_true",
-        help="Flag to not split input into multiple entries at all."
+        help="Flag to display debug info"
     )
     parser.add_argument(
-        "-is",
-        "--input_separator",
-        type=str,
-        default=_DEFAULT_INPUT_LINE_SEPARATOR,
-        help="String to separate input data.",
-    )
-    parser.add_argument(
-        "-os",
-        "--output_separator",
-        type=str,
-        default=_DEFAULT_OUTPUT_LINE_SEPARATOR,
-        help="String to use to join separated input data beforseparatore printing to console.",
-    )
-    parser.add_argument(
-        "-sw",
-        "--split_whitespace",
+        "-bf",
+        "--blank-line-filtered",
         action="store_true",
-        help="Flag to override `input_separator` and split input data on all whitespace.",
+        help="Flag to specify if a blank line should be showed for filtered rows"
     )
     parser.add_argument(
-        "-s",
-        "--separator",
-        type=str,
-        help="String to separate both input and output data.  If specified, overrides both `--input_separator` and `--output_separator`.",
+        "-be",
+        "--blank-line-exceptions",
+        action="store_true",
+        help="Flag to specify if a blank line should be showed for exception rows"
     )
     parser.add_argument(
         "-se",
-        "--split_entry",
-        type=str,
-        help="String to split data entries on if data entries are to be split.",
-    )
-    parser.add_argument(
-        "-act",
-        "--arg_container_type",
-        type=type,
-        default=tuple,
-        help="The container type to split entries by `--split_entry` into.",
-    )
-    parser.add_argument(
-        "-ct",
-        "--container_type",
-        type=str,
-        default="list",
-        help="Container to collect elements in.",
-    )
-    parser.add_argument(
-        "-m",
-        "--modules",
-        type=str,
-        nargs="*",
-        help="Modules from the current Python execution environment to import before evaluating.  Calling `--module X` will import X as a module, Calling `--module X:Y` will import object Y from module X, and `--module X:Y:Z` will import object Y from module X as Z",
-    )
-    parser.add_argument(
-        "-t",
-        "--type",
-        type=str,
-        default="str",
-        help="Data type to convert entries into prior to apply executing.",
-    )
-    parser.add_argument(
-        "-rd",
-        "--reduce_default",
-        help="Default value to use when aggregating via reduce.",
-    )
-    parser.add_argument(
-        "-ji",
-        "--json_in",
+        "--show-exceptions",
         action="store_true",
-        help="Flag to specify if input should be parsed as JSON.",
+        help="Flag to print exception messages into stderr"
     )
     parser.add_argument(
-        "-jo",
-        "--json_out",
+        "-sf",
+        "--show-filtered",
         action="store_true",
-        help="Flag to specify if output should be parsed as JSON",
+        help="Flag to print error into stderr"
     )
     parser.add_argument(
-        "-j",
-        "--json",
+        "-e",
+        "--expand",
         action="store_true",
-        help="Flag to specify that both input and output should be parsed as JSON.",
+        help="Flag to expand collected elements back into individual records prior to sending back to stdout/stderr"
     )
-    parser.add_argument(
-        "-jos",
-        "--json_out_sort",
-        action="store_true",
-        help="Flag to specify whether output JSON will be sorted by key or not.",
-    )
-    parser.add_argument(
-        "-joi",
-        "--json_out_indent",
-        type=str,
-        help="Specify indentation for output JSON.",
-    )
-    parser.add_argument("-i", "--init", type=str, nargs="*", help="Commands to run prior to start to prep the environment and create any needed resources.")
-    args = parser.parse_args()
 
-    if (args.fn in "version") or (args.version):
-        print(f"{_CMD_NAME} - v_{_CMD_VERSION}")
+    parsed = parser.parse_args()
+    namespace = {
+        **parse_imports(parsed.imports, Imports.module),
+        **parse_imports(parsed.constants, Imports.variable),
+    }
+    parsed.collection = Collection.infer(parsed.collect)
+    parsed.namespace = namespace
+    return parsed
+
+
+# i know the below function can be made prettier by abstracting out a bunch of
+# the logic (ie, the actual evaluation part, splitting into two other functions
+# where one handles collections and the other handles evaluations, etc),
+# but due to the heavy performance hit incurred by function
+# calls python and my reluctance to duplicate the same code in two separate
+# functions in case it needs to be changed i've just thrown it together as you
+# see below.
+#
+# sue me
+def evaluate(collection: Iterable, args: Namespace, return_evaluation: bool = False) -> list[Record] | None:
+    if args.debug:
+        print("NAMESPACE:", args, "\n")
+    records = []
+    if args.collection:
+        collected = args.collection.value(element.strip() for element in collection)
+        collection = [collected]
+    for element in collection:
+        element = element if not isinstance(element, str) else element.strip()
+        record = Record(val=element, status=Status.VALID)
+        # the below for-loop evaluates the output value of each input element across all maps/filters at one time
+        for act, fn_str in args.ordered_args:
+            try:
+                # fn = lambda _: eval(fn_str, {"_": _, **args.namespace})
+                # output = fn(record.val)
+                output = eval(fn_str, {args.variable_repr: record.val, **args.namespace})
+                if args.debug:
+                    print(f"VALID: {record = }, {output = }, {fn_str = }")
+            except Exception as e: # skip excepted elements
+                record = record.update(val=e, status=Status.ERROR)
+                if args.debug:
+                    print(f"EXCEPTION: {record = }, {act = }, {fn_str = }\n")
+                break
+            if act == "filter": # skip filtered elements or pass through previous evaluated value
+                if not output:
+                    record = record.update(val=output, status=Status.FILTER)
+                    if args.debug:
+                        print(f"FILTERED: {record = }, {act = }, {fn_str = }\n")
+                    break
+            else: # pass through valid elements that weren't filtered
+                record = record.update(val=output)
+        new_records = [record]
+        if args.collection and args.expand:
+            new_records = [Record(val=v, status=Status.VALID) for v in record.val]
+        if not return_evaluation:
+            for new_record in new_records:
+                match new_record.status:
+                    case Status.ERROR:
+                        if args.show_exceptions:
+                            print(new_record.val, file=sys.stderr)
+                        if args.blank_line_exceptions:
+                            print("", file=sys.stdout)
+                    case Status.FILTER:
+                        if args.show_filtered:
+                            print(new_record.val, file=sys.stderr)
+                        if args.blank_line_filtered:
+                            print("", file=sys.stdout)
+                    case _:
+                        if args.debug:
+                            print(f"OUTPUT: {new_record.val}\n", file=sys.stdout)
+                        else:
+                            print(new_record.val, file=sys.stdout)
+        else:
+            records = records + new_records
+    return records
+
+
+def main():
+    args = parse_args()
+    if args.version:
+        print(f"fnk {metadata.version('fnk')}")
     else:
-        # Import modules
-        if args.modules is not None:
-            for module in args.modules:
-                split_module = module.split(":")
-                match len(split_module):
-                    case 1:
-                        exec(f"import {module}")
-                    case 2:
-                        exec(f"from {split_module[0]} import {split_module[1]}")
-                    case 3:
-                        exec(
-                            f"from {split_module[0]} import {split_module[1]} as {split_module[3]}"
-                        )
-        # Execute command
-        Fn(args).evaluate()
+        evaluate(sys.stdin, args)
+
 
 if __name__ == "__main__":
     main()
